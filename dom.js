d425e270005a9c131046ee30defcffa254e82926
Edikan <edikanakpan5@gmail.com>
2022-06-23 14:34:15 +0100

    first commit

---
+++ dom.js
@@ -0,0 +1,12 @@
+var fullName = document.querySelector(".name");
+fullName.textContent= "Name: Edikan Friday Akpan"
+var heightInFt = document.querySelector(".height");
+heightInFt.textContent= "Height: 6ft"
+var countryName = document.querySelector('.country');
+countryName.textContent = 'Country: Nigeria'
+
+
+
+
+
+
